07498f079ad0969e2276c5e1b76c32f0c541a799
Alisa1234567890-78 <alisa.bugayova@mail.ru>
2026-05-08 11:14:07 +0500

    Update and rename _0.js to d0.js

--- data/d0.js
+++ data/d0.js
@@ -1,6 +1,6 @@
 var json__0 = {
 "type": "FeatureCollection",
-"name": "_0",
+"name": "d0",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
 { "type": "Feature", "properties": { "fid": "Автоматически" }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
